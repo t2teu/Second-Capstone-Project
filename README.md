8acaf1fb246eccd205e2a3c52af8150b38858d34
teteu <msouza11302003@gmail.com>
2023-05-24 16:54:10 -0300

    Update v1.0.1
    Fixed some bugs as well as finished the README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Second-Capstone-Project
- Second Capstone project for Mailjard's class. A website for an outdoors activity page.
+Week 7 Capstone Project of "The Town Map", a website designed to provide comprehensive information and resources related to mountainous regions and America's national parks at the click of a button.
+
+(To download my code, click on the Release link up and to the right)
+
+In this project, I created "The Town Map", a website that enables users to search mountains to hike or national parks to visit is an online platform designed to provide comprehensive information and resources related to outdoor recreational activities in mountainous regions and national parks. This website serves as a valuable tool for individuals who are passionate about hiking, nature exploration, and adventure.
+
+There are 4 main pages in the website.
+
+The home page
+The mountains search page
+The national parks search page
+An easter egg page
+
+All the images in the website, are either assets from Pokemon HeartGold and Pokemon Emerald, or assets that have been provided to me by my instructor.
+
+I'm very proud of the branding, as it wasn't designed to be a normal page that can be searched through Google, but more of word-of-mouth, niche website that would give people comfort.
+
+![HOMEPAGE](images/page1.png)
+![NATIONALPARKSPAGE](images/page2.png)
+![MOUNTAINSPAGE](images/page3.png)
--- scripts/bird.js
+++ scripts/bird.js
@@ -1,31 +1,38 @@
 const linksWithBird = document.querySelectorAll('.link-with-bird');
 
 linksWithBird.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
+  let birdAppended = false; // Flag to track if the bird has been appended
 
-        const birdImage = document.createElement('img');
-        birdImage.src = 'images/bird.png';
-        birdImage.alt = 'Bird';
-        birdImage.classList.add('bird-image');
-        link.appendChild(birdImage);
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
 
-        setTimeout(() => {
-            window.location.href = link.href;
-        }, 2000);
-    });
+    if (!birdAppended) { // Check if the bird has not been appended yet
+      const birdImage = document.createElement('img');
+      birdImage.src = 'images/bird.png';
+      birdImage.alt = 'Bird';
+      birdImage.classList.add('bird-image');
+      link.appendChild(birdImage);
+
+      birdAppended = true; // Set the flag to indicate that the bird has been appended
+    }
+
+    setTimeout(() => {
+      playFlySound(); // Play the fly sound
+      window.location.href = link.href;
+    }, 2000);
+  });
 });
 
-   function playFlySound() {
-    var flySound = new Audio("images/fly.mp3");
-    flySound.addEventListener("canplaythrough", function() {
-      flySound.volume = 0.5;
-      flySound.play();
-    });
-  }
-
-  // Add click event listeners to the red-square buttons
-  var redSquareButtons = document.querySelectorAll(".img-fluid");
-  redSquareButtons.forEach(function(button) {
-    button.addEventListener("click", playFlySound);
+function playFlySound() {
+  var flySound = new Audio("images/fly.mp3");
+  flySound.addEventListener("canplaythrough", function() {
+    flySound.volume = 0.5;
+    flySound.play();
   });
+}
+
+
+const redSquareButtons = document.querySelectorAll(".img-fluid");
+redSquareButtons.forEach(function(button) {
+  button.addEventListener("click", playFlySound);
+});
